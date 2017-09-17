24dc22319f86484af8ced387882eea522417378c
James Maher <james.maher@digitas.com>
2017-09-16 22:34:05 -0400

    Add ( C ) for captain to Starting XI

--- app/src/StartingXI.js
+++ app/src/StartingXI.js
@@ -12,12 +12,14 @@ class StartingXI extends Component {
   }
   render() {
     let mainState = this.props.mainState;
+    var captainIndex = Math.floor((Math.random() * 11));
     return (
       <div className="Starters">
           <ul>
             {
-              mainState.squad.map(function(player){
-                return <li key={'num' + player.number}> {player.name} </li>
+              mainState.squad.map(function(player, i){
+                let classAdd = captainIndex == i ? 'captain' : null
+                return <li className={classAdd} key={'num' + player.number}> {player.name} </li>
               })
             }
           </ul>
