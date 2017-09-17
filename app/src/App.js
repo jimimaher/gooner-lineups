f3a8dfeb4e82b59afd00736de79b3dafa24e80ca
James Maher <james.maher@digitas.com>
2017-09-16 23:22:26 -0400

    Intro paragraph for context

--- app/src/App.js
+++ app/src/App.js
@@ -71,6 +71,11 @@ class App extends Component {
         />
     }
 
+    let renderInfo = null;
+    if( this.state.squad.length === 0 ){
+      renderInfo = <h3 className="intro">We have finally cracked the case on how Wenger makes team selections. He chooses at random! Now you can choose Arsenal's starting lineup just line Arsene!</h3>
+    }
+
     let renderReset = null;
     if( this.state.squad.length === 11 && this.state.formation != null ){
       renderReset = <button className="medium" onClick={this.resetState}>Start Over</button>
@@ -80,10 +85,14 @@ class App extends Component {
       <div>
           <div id="bg"></div>
           <h1>{ this.state.squad.length !== 0 ? 'Today\'s XI:' : 'Gooner Lineups' }</h1>
+          {renderInfo}
           <button className="medium" onClick={this.generateXI}> { this.state.squad.length === 0 ? 'Build XI' : 'Tinker' } </button>
           {renderStarters}
           {renderTactical}
           {renderReset}
+
+          <p>No affiliation with Arsenal FC</p>
+
       </div>
     );
   }
